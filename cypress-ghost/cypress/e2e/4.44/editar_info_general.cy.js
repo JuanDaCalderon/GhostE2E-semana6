1dfd42ba4ff02b350638de4c2cbec4b3b4442180
IngSneyderAmado <s.amadov@uniandes.edu.co>
2023-05-09 21:09:15 -0500

    se hicieron los cambios de login y comentarios

--- cypress-ghost/cypress/e2e/4.44/editar_info_general.cy.js
+++ cypress-ghost/cypress/e2e/4.44/editar_info_general.cy.js
@@ -1,29 +1,49 @@
 import configJson from '../../../config/config.json';
 describe('edit info general', () => {
     it('Editar informacion titulo y descripcion de la pagina ', () => {
+
+      //Given I navigate to page "http://localhost:3002/ghost"  
       cy.visit(configJson.host4_44)
       cy.wait(2000)
+
+      //When I enter email "<Usuario1>"
       cy.get('input[name="identification"]').type(configJson.user)
       cy.wait(2000)
+
+      //And I enter password "<Password1>"
       cy.get('input[name="password"]').type(configJson.password)
       cy.wait(2000)
+
+      //And I click next
       cy.get('button[class="login gh-btn gh-btn-login gh-btn-block gh-btn-icon js-login-button ember-view"]').click()
       cy.wait(2000)
       cy.screenshot("editar_info_general_p1");
+
+      //And I click settings general
       cy.get('a[href="#/settings/general/"]').click()
       cy.wait(2000)
       cy.screenshot("editar_info_general_p2");
+
+      //And I click Expand 
       cy.get('button[data-ember-action-65="65"]').click()
       cy.wait(2000)
       cy.screenshot("editar_info_general_p3");
+
+      //And I enter name user
       cy.get('#ember100').clear().type('pruebas automatizadas 3',{force: true})
       cy.wait(2000)
+
+      //And I entre description
       cy.get('#ember102').clear().type('descripicio de pruebas automatizadas',{force: true})
       cy.wait(2000)
       cy.screenshot("editar_info_general_p4");
+
+      //And I click Save settings
       cy.get('button[class="gh-btn gh-btn-blue gh-btn-icon ember-view"]').click()
       cy.wait(2000)
       cy.screenshot("editar_info_general_p5");
+
+      //And I click Expand 
       cy.get('button[data-ember-action-65="65"]').click()
       cy.wait(2000)
       cy.screenshot("editar_info_general_p6");
