98e377ca71d370f54db346da4e6ed4c427f528d1
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2023-05-12 16:47:04 -0500

    Se actualizan funciones para generar los reportes de comparación de screenshots

--- VRT-resemble/index.js
+++ VRT-resemble/index.js
@@ -20,3 +20,67 @@ async function executeTestForCrearPostBorrador() {
   return await crear_post_borrador.compareImages();;
 }
 (async () => console.log(await executeTestForCrearPostBorrador()))();
+
+// 3. Crear tag
+async function executeTestForCrearTag() {
+  const crear_tag = new Compare(screenshotsRoot, 'crear-tag', browsers, options, V3_41_1, V4_44)
+  if (!crear_tag.checkBrowsers()) return;
+  return await crear_tag.compareImages();;
+}
+(async () => console.log(await executeTestForCrearTag()))();
+
+// 4. Eliminar tag
+async function executeTestForEliminarTag() {
+  const eliminar_tag = new Compare(screenshotsRoot, 'eliminar-tag', browsers, options, V3_41_1, V4_44)
+  if (!eliminar_tag.checkBrowsers()) return;
+  return await eliminar_tag.compareImages();;
+}
+(async () => console.log(await executeTestForEliminartag()))();
+
+// 5. Crear page
+async function executeTestForCrearPage() {
+  const crear_page = new Compare(screenshotsRoot, 'crear-page', browsers, options, V3_41_1, V4_44)
+  if (!crear_page.checkBrowsers()) return;
+  return await crear_page.compareImages();;
+}
+(async () => console.log(await executeTestForCrearPage()))();
+
+// 6. Crear post publicado
+async function executeTestForCrearPostPublicado() {
+  const crear_post_publicado = new Compare(screenshotsRoot, 'crear-post-publicado', browsers, options, V3_41_1, V4_44)
+  if (!crear_post_publicado.checkBrowsers()) return;
+  return await crear_post_publicado.compareImages();;
+}
+(async () => console.log(await executeTestForCrearPostPublicado()))();
+
+// 7. Eliminar post
+async function executeTestForEliminarPost() {
+  const eliminar_post = new Compare(screenshotsRoot, 'eliminar-post', browsers, options, V3_41_1, V4_44)
+  if (!eliminar_post.checkBrowsers()) return;
+  return await eliminar_post.compareImages();;
+}
+(async () => console.log(await executeTestForEliminarPost()))();
+
+// 8. Ver internal tag
+async function executeTestForVerInternalTag() {
+  const ver_internal_tag = new Compare(screenshotsRoot, 'ver-internal-tag', browsers, options, V3_41_1, V4_44)
+  if (!ver_internal_tag.checkBrowsers()) return;
+  return await ver_internal_tag.compareImages();;
+}
+(async () => console.log(await executeTestForVerInternalTag()))();
+
+// 9. Invitar usuario
+async function executeTestForInvitarUsuario() {
+  const invitar_usuario = new Compare(screenshotsRoot, 'invitar-usuario', browsers, options, V3_41_1, V4_44)
+  if (!invitar_usuario.checkBrowsers()) return;
+  return await invitar_usuario.compareImages();;
+}
+(async () => console.log(await executeTestForInvitarUsuario()))();
+
+// 10. Private page general
+async function executeTestForPrivatePageGeneral() {
+  const private_page_general = new Compare(screenshotsRoot, 'private-page-general', browsers, options, V3_41_1, V4_44)
+  if (!private_page_general.checkBrowsers()) return;
+  return await private_page_general.compareImages();;
+}
+(async () => console.log(await executeTestForPrivatePageGeneral()))();
